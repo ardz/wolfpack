8cce6bfd2c013fa158b16b5e9f4c3abcf30633c9
snazzle <richard@developerintest.dev>
2023-10-14 22:27:45 +0100

    added tests, create POM

--- js/test/test.js
+++ js/test/test.js
@@ -1,61 +1,77 @@
-// add required packages
-const assert = require('chai').assert;
 const { remote } = require('webdriverio');
+const assert = require('chai').assert;
 
-// add the main screen page object
-const mainScreen = require('../screens/mainScreen');
+const ClickMeScreen = require('../screens/MainScreen.js');
 
-// define appium capabilities
-const capabilities = {
-    platformName: 'Android',
-    'appium:automationName': 'UiAutomator2',
-    'appium:deviceName': 'Android',
-    //'appium:appPackage': 'com.android.settings',
-    //'appium:appActivity': '.Settings',
-};
+let driver = null;
+let mainScreen = null;
 
-const webdriverOptions = {
+//TODO move to config file
+const remoteOptions = {
     hostname: process.env.APPIUM_HOST || '127.0.0.1',
     port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
     logLevel: 'info',
-    capabilities,
+    capabilities: {
+        platformName: 'Android',
+        'appium:automationName': 'UiAutomator2',
+        'appium:deviceName': 'Android',
+        'appium:appPackage':'com.palringo.qa_automation_android',
+        'appium:appActivity': '.MainActivity',
+        'appium:fastReset': true
+    }
 };
 
-let driver;
-
-// Before all tests, initialise driver
 before(async function() {
-    this.timeout(10000);  // setting timeout to 10 seconds for this hook
-    driver = await remote(webdriverOptions);
-    // Add any additional setup steps if required
+    this.timeout(10000); // set timeout to 10 seconds
+    driver = await remote(remoteOptions);
+    mainScreen = new ClickMeScreen(driver);
 });
 
-// After all tests, close driver session
 after(async function() {
-    if (driver) {
+    try {
         await driver.deleteSession();
+    } catch(err) {
+        console.error("Error while deleting the session:", err);
     }
 });
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
+describe('Button Click Count Feature Acceptance Test', function() {
 
-describe('Can open the battery app', function() {
     this.timeout(10000);
-    it('should find the battery item and click it', async function() {
-        try {
-            await driver.startActivity("com.android.settings",".Settings");
-            const batteryItem = await driver.$('//*[@text="Battery"]');
-            await batteryItem.click();
-        } finally {
-            await driver.pause(1000);
-        }
-    })
+
+    // Scenario: Click me screen opens
+    // WHEN the app is opened
+    // THEN its main screen contains a button with text “Click me” and a label with text “0”
+    it('should display a button with text "Click me" and a label with text "0"', async function() {
+        assert.equal(await mainScreen.getButtonText(), 'Click me', 'The clickValue did not match expected value');
+        assert.equal(await mainScreen.getLabelText(), '0', 'Label did not match expected')
+    });
+
+    // Scenario: Click button when label is "0"
+    // GIVEN the label is "0"
+    // WHEN clicking the button
+    // THEN the label should change to "1"
+    it('should update the label to "1" when the label is "0" and the button is clicked', async function() {
+        await mainScreen.clickButton();
+        assert.equal(await mainScreen.getLabelText(), '1', 'Label did not match expected')
+    });
+
+    // Scenario: Click button when label is "1"
+    // GIVEN the label is "1"
+    // WHEN clicking the button
+    // THEN the label should change to "2"
+    it('should update the label to "2" when the label is "1" and the button is clicked', async function() {
+        await mainScreen.clickButton();
+        assert.equal(await mainScreen.getLabelText(), '2', 'Label did not match expected')
+    });
+
+    // Scenario: Click button when label is "2"
+    // GIVEN the label is "2"
+    // WHEN clicking the button
+    // THEN the label should change to "MAX"
+    it('should update the label to "MAX" when the label is "2" and the button is clicked', async function() {
+        await mainScreen.clickButton();
+        assert.equal(await mainScreen.getLabelText(), 'MAX', 'Label did not match expected')
+    });
+
 });
-
-
